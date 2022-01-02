8f44e1de4471bcfd4a6293237763785cd5d5bde3
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-01-02 19:19:37 +0100

    Creating Ingredient, Notes, Recipe and UnitOfMeasure entities

---
+++ src/main/java/fr/hndgy/recipeapp/entities/Ingredient.java
@@ -0,0 +1,21 @@
+package fr.hndgy.recipeapp.entities;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+}
