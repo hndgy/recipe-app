f356f8e4df12c000c674012e0462727f6f248756
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-01-15 15:04:02 +0100

    Integration Testing RecipeService

---
+++ src/test/java/fr/hndgy/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package fr.hndgy.recipeapp.services;
+
+import fr.hndgy.recipeapp.domain.Recipe;
+import fr.hndgy.recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();// verifier qu'on appelle bien 1 fois le mock
+    }
+}
